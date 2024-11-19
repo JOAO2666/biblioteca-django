eb92164cbf0d76447c2ab067df89cf554437481e
JOAO2666 <20201INTADM0050@ifba.edu.br>
2024-11-19 18:31:32 -0300

    Final adjustments and resolved conflicts

--- manage.py
+++ manage.py
@@ -3,14 +3,9 @@
 import os
 import sys
 
-
 def main():
     """Run administrative tasks."""
-<<<<<<< HEAD
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'biblioteca.settings')
-=======
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restful01.settings')
->>>>>>> cbee2eaf6fcc7ea040c4ec830cc9ed5a858dd459
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -20,7 +15,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
